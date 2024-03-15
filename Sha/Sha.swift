0973b2aaeac45cbdf69f24819d716c6ae0105018
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2024-03-15 11:05:02 +0100

    hmmmm

---
+++ Sha/Sha.swift
@@ -0,0 +1,35 @@
+//
+//  Sha.swift
+//  Sha
+//
+//  Created by Kiara on 15.03.24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoTimer: App {
+
+    
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                Text("Aloha")
+            }
+            
+           /* ZStack(alignment: .top) {
+                TabbarView()
+                ToastView()
+            }
+            .environment(\.managedObjectContext, CoreDataStack.sharedContext)
+            .environmentObject(toastControl)
+            .onAppear {
+                UserDefaults.standard.register(defaults: [UD_repairData: false])
+                UserDefaults.standard.register(defaults: [UD_firstLaunchKey: true])
+                UserDefaults.standard.register(defaults: [UD_lastVersion: "0.0.0"])
+                UserDefaults.standard.register(defaults: [UD_lastAskedForeReview: "\(Date().timeIntervalSince1970)"])
+                UserDefaults.standard.register(defaults: [UD_lastbackup: ""])
+            }*/
+        }
+    }
+}
